26a7c10e532e0549b6e7f0e8e31959bbe6371334
Mavi Toktamis-Paker <mavisansmembres@gmail.com>
2020-01-21 19:31:32 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,10 @@
 # My Code here....
+def map_to_negativize(source_array)
+  output = Array.new
+  index = 0
+  while index < source_array.length
+    output << 0 - source_array[index]
+    index += 1
+  end
+  return output
+end
